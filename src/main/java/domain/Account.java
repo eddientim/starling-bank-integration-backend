0c40f3938d10dcc2db55710f8aabd10c5fd88f98
eddientim <eddie.ntim@thoughtworks.com>
2023-01-09 22:43:08 +0000

    add domain for retrieving account information

---
+++ src/main/java/domain/Account.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+  private List<AccountDetails> accounts;
+
+}
---
+++ src/main/java/domain/AccountDetails.java
@@ -0,0 +1,21 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class AccountDetails {
+
+  private String accountUid;
+  private AccountType accountType;
+  private String defaultCategory;
+  private String currency;
+  private String createdAt;
+  private String name;
+
+}
