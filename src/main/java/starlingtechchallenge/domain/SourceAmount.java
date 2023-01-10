da18111181ceb83aac447e6a9f6b5e7537b0e74c
eddientim <eddie.ntim@thoughtworks.com>
2023-01-10 01:44:01 +0000

    change data type for minor units

--- src/main/java/starlingtechchallenge/domain/SourceAmount.java
+++ src/main/java/starlingtechchallenge/domain/SourceAmount.java
@@ -1,10 +1,12 @@
 package starlingtechchallenge.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class SourceAmount {
 
   private String currency;
-  private String minorUnits;
+  private int minorUnits;
 }
