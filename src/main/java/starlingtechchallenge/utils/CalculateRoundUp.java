81e106a028612f86adb395330f0c20aaac4f55e5
eddientim <edward11r@yahoo.co.uk>
2023-10-02 22:48:44 +0100

    refactor code to calculate multiple amount sums

--- src/main/java/starlingtechchallenge/utils/CalculateRoundUp.java
+++ src/main/java/starlingtechchallenge/utils/CalculateRoundUp.java
@@ -1,10 +1,12 @@
 package starlingtechchallenge.utils;
 
-import java.util.List;
 import org.springframework.stereotype.Service;
 import starlingtechchallenge.domain.Amount;
+import starlingtechchallenge.domain.Transaction;
 import starlingtechchallenge.domain.TransactionFeed;
 import starlingtechchallenge.exception.NoTransactionFoundException;
+
+import java.util.List;
 
 @Service
 public class CalculateRoundUp {
@@ -20,14 +22,27 @@ public class CalculateRoundUp {
 
     transactions.noTransactions();
 
+    if (filterOutGoingTransactions(transactions)) {
     final int sum = transactions.getFeedItems().stream()
-        .filter(item -> item.getDirection().equals("OUT"))
         .mapToInt(item -> item.getAmount().getMinorUnits())
         .filter(amount -> amount >= 0)
         .map(amount -> 100 - amount % 100)
         .filter(amount -> amount != 100)
         .sum();
-    final String currency = transactions.getFeedItems().get(0).getAmount().getCurrency();
+    final String currency = getCurrency(transactions.getFeedItems());
     return Amount.builder().currency(currency).minorUnits(sum).build();
+    }
+    return Amount.builder().build();
+  }
+
+  private String getCurrency(List<Transaction> transactions) {
+    return transactions.stream()
+            .findAny()
+            .orElseThrow()
+            .getAmount()
+            .getCurrency();
+  }
+  private boolean filterOutGoingTransactions(TransactionFeed feedItems) {
+     return feedItems.getFeedItems().stream().anyMatch(tr -> tr.getDirection().equalsIgnoreCase("OUT"));
   }
 }
--- src/test/java/starlingtechchallenge/utils/CalculateRoundUpTest.java
+++ src/test/java/starlingtechchallenge/utils/CalculateRoundUpTest.java
@@ -2,36 +2,61 @@ package starlingtechchallenge.utils;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import starlingtechchallenge.domain.Amount;
 import starlingtechchallenge.domain.Transaction;
 import starlingtechchallenge.domain.TransactionFeed;
+import starlingtechchallenge.domain.response.AllSavingsGoalDetails;
 import starlingtechchallenge.exception.NoTransactionFoundException;
+import starlingtechchallenge.gateway.AccountGateway;
+import starlingtechchallenge.gateway.TransactionFeedGateway;
+import starlingtechchallenge.service.RoundUpService;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
+import static java.time.OffsetDateTime.now;
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static starlingtechchallenge.helpers.DataBuilders.accountData;
+import static starlingtechchallenge.helpers.DataBuilders.transactionFeedData;
 
 @SpringBootTest
 public class CalculateRoundUpTest {
 
-  @Autowired
+  private static final String ACCOUNT_ID = "someAccountId";
+  private final static String DEFAULT_CATEGORY_UID = "some-category-uid";
+  private final static OffsetDateTime OFFSET_DATE_TIME = now();
+  private final static OffsetDateTime DATE_TIME_TO = now();
+  @Mock
+  private RoundUpService roundUpService;
+  @Mock
+  private AccountGateway accountGateway;
+  @Mock
+  private TransactionFeedGateway transactionFeedGateway;
+
+  @InjectMocks
   private CalculateRoundUp calculateRoundUp;
 
   @Test
   public void shouldCalculateRoundUp() {
     Amount amount = Amount.builder().currency("GBP").minorUnits(10).build();
     Transaction feedItem = Transaction.builder().direction("OUT").amount(amount).build();
-    TransactionFeed feedItems = TransactionFeed.builder().feedItems(List.of(feedItem))
-        .build();
+    TransactionFeed feedItems = TransactionFeed.builder().feedItems(List.of(feedItem)).build();
+    Amount expectedAmount = Amount.builder().currency("GBP").minorUnits(90).build();
+
+    AllSavingsGoalDetails allSavingsGoalDetails = new AllSavingsGoalDetails();
+
+    when(roundUpService.calculateRoundUp(ACCOUNT_ID, OffsetDateTime.now(), OffsetDateTime.now())).thenReturn(allSavingsGoalDetails);
+    when(transactionFeedGateway.getTransactionFeed(ACCOUNT_ID, DEFAULT_CATEGORY_UID, OFFSET_DATE_TIME, DATE_TIME_TO)).thenReturn(transactionFeedData());
+    when(accountGateway.retrieveCustomerAccounts()).thenReturn(accountData());
 
     Amount actualAmount = calculateRoundUp.roundUp(feedItems);
 
-    Amount expectedAmount = Amount.builder().currency("GBP").minorUnits(90).build();
-
-    Assertions.assertEquals(expectedAmount, actualAmount);
+    assertEquals(expectedAmount, actualAmount);
   }
 
   @Test
